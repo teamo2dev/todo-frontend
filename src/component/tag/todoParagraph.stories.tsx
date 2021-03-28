c42417e700bb4a97d78c5a9eaa2fcf26f9f25764
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-03-28 20:15:12 +0900

    Implement Todo's P Component (w/ StyledComponent)

---
+++ src/component/tag/todoParagraph.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta } from "@storybook/react/types-6-0";
+
+import { TodoParagraph } from "./todoParagraph";
+
+export default {
+    title: "Component/tag/TodoParagraph",
+    component: TodoParagraph,
+} as Meta;
+
+export const Default = () => <TodoParagraph string={"Default TodoParagraph String"} />
+
+export const DefaultKorean = () => <TodoParagraph string={"기본 TodoParagraph 문구"} />
---
+++ src/component/tag/todoParagraph.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ITodoParagraphProps {
+    string: string;
+}
+
+const TodoParagraphStyled = styled.p`
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&family=Poor+Story&display=swap');
+  font-family: 'Nunito', 'Poor Story', sans-serif;
+`;
+
+export const TodoParagraph: React.FC<ITodoParagraphProps> = ({ string }) => {
+    return <TodoParagraphStyled >{ string }</TodoParagraphStyled>
+};
