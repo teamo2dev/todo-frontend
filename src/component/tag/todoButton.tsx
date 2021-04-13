aa4708564b853c39f469a8d75403fb5dbea0dca7
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-04-13 19:59:47 +0900

    Setting Eslint & Prettier

--- src/component/tag/todoButton.tsx
+++ src/component/tag/todoButton.tsx
@@ -9,23 +9,23 @@ interface ITodoButtonProps {
 }
 
 const TodoButtonStyled = styled.button`
-  background: ${props => props.color};
-  border: 1px solid lightgray;
-  border-radius: 0.3rem;
-  cursor: pointer;
+    background: ${(props) => props.color};
+    border: 1px solid lightgray;
+    border-radius: 0.3rem;
+    cursor: pointer;
 
-  &:focus {
-    outline: none;
-    border: 1px solid rgb(65, 105, 225);
-    box-shadow: 0 0 5px 0 rgb(65, 105, 225);
-  }
+    &:focus {
+        outline: none;
+        border: 1px solid rgb(65, 105, 225);
+        box-shadow: 0 0 5px 0 rgb(65, 105, 225);
+    }
 `;
 
 export const TodoButton: React.FC<ITodoButtonProps> = ({ string, bgColor }) => {
     const color = bgColor ? bgColor : 'rgb(255, 255, 250)';
     return (
-        <TodoButtonStyled color={ color } >
-            <TodoParagraph string={ string } />
+        <TodoButtonStyled color={color}>
+            <TodoParagraph string={string} />
         </TodoButtonStyled>
-    )
+    );
 };
