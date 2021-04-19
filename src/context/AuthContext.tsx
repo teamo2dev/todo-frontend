4a96ea6ad20cba8850ce2dd27c858925e098c97d
stevejkang <iam@juneyoung.io>
2021-04-19 22:54:03 +0900

    Create private route and implement auth state

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,54 @@
+import React, { useReducer, Dispatch } from 'react';
+
+type AuthStateType = {
+    isAuthenticated: boolean;
+};
+
+type AuthActionType = 'signIn' | 'signOut';
+
+type AuthAction = {
+    type: AuthActionType;
+    payload: AuthStateType;
+};
+
+const defaultAuthValue: AuthStateType = {
+    isAuthenticated: Boolean(localStorage.getItem('token')),
+};
+
+export const AuthStateContext = React.createContext<AuthStateType>(
+    defaultAuthValue
+);
+export const AuthDispatchContext = React.createContext<
+    Dispatch<AuthActionType>
+>(() => null);
+
+const authReducer = (
+    state: AuthStateType,
+    action: AuthAction
+): AuthStateType => {
+    if (!action.type) {
+        return { isAuthenticated: false };
+    }
+
+    return {
+        ...state,
+        isAuthenticated: action.payload.isAuthenticated,
+    };
+};
+
+type AuthProviderProps = {
+    children: React.ReactChild;
+};
+
+export const AuthProvider = (props: AuthProviderProps) => {
+    const { children } = props;
+    const [state, dispatch] = useReducer(authReducer, defaultAuthValue);
+
+    return (
+        <AuthStateContext.Provider value={state}>
+            <AuthDispatchContext.Provider value={dispatch}>
+                {children}
+            </AuthDispatchContext.Provider>
+        </AuthStateContext.Provider>
+    );
+};
